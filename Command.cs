68edc41937c6514fe96580f124fc9eeb9202228d
exys228 <exys228@gmail.com>
2018-10-31 04:16:02 +0300

    Added main part

---
+++ Command.cs
@@ -0,0 +1,22 @@
+﻿namespace exys228.HopeCommands
+{
+	public class Command
+	{
+		public delegate void DCallback(string[] args);
+
+		private DCallback Callback;
+
+		public string CommandText { get; private set; }
+
+		public string Description { get; private set; }
+
+		public Command(string command, string description, DCallback callback)
+		{
+			CommandText = command;
+			Description = description;
+			Callback = callback;
+		}
+
+		public void Execute(string[] args) => Callback(args); // todo: version without args
+	}
+}
